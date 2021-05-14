5f75f7d05865780347d207f82aa0f0b11e326787
Petri Silen <petri.silen@silensoft.com>
2021-05-14 18:48:38 +0300

    Added annotation

--- src/types/id/Id.ts
+++ src/types/id/Id.ts
@@ -1,8 +1,12 @@
-import MaxLengthAndMatches from '../../decorators/typeproperty/MaxLengthAndMatches';
-import IsStringOrObjectId from '../../decorators/typeproperty/IsStringOrObjectId';
+import MaxLengthAndMatches from "../../decorators/typeproperty/MaxLengthAndMatches";
+import IsStringOrObjectId from "../../decorators/typeproperty/IsStringOrObjectId";
+import ReadWrite from "../../decorators/typeproperty/ReadWrite";
+import { NotUnique } from "../../index";
 
 export default class Id {
   @IsStringOrObjectId()
   @MaxLengthAndMatches(24, /^[a-f\d]{1,24}$/)
+  @NotUnique()
+  @ReadWrite()
   public id!: string;
 }

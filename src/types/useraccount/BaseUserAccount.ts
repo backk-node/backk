1fb516cda3a0aa3f24329f6bd18ec62246ca57c9
Petri Silen <petri.silen@silensoft.com>
2021-09-07 11:14:35 +0300

    Modified

--- src/types/useraccount/BaseUserAccount.ts
+++ src/types/useraccount/BaseUserAccount.ts
@@ -1,16 +1,18 @@
 import _IdAndCaptcha from "../id/_IdAndCaptcha";
 import Unique from "../../decorators/typeproperty/Unique";
-import { IsString, MaxLength } from "class-validator";
+import { IsAscii, IsString, MaxLength } from "class-validator";
 import Private from "../../decorators/typeproperty/Private";
 import IsUndefined from "../../decorators/typeproperty/IsUndefined";
+import ReadWrite from "../../decorators/typeproperty/ReadWrite";
 import IsSubject from "../../decorators/typeproperty/IsSubject";
 
 export default class BaseUserAccount extends _IdAndCaptcha  {
-  @IsUndefined({ groups: ['__backk_create__', '__backk_update__'] })
+  @IsUndefined({ groups: ['__backk_update__'] })
   @Unique()
   @IsString()
   @MaxLength(255)
   @IsSubject()
+  @ReadWrite()
   @Private()
-  readonly subject!: string;
+  subject!: string;
 }

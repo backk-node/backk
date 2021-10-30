bf9a5eca567ff2de9a103f8e1b0cf7fd3eedcc44
Petri Silen <petri.silen@silensoft.com>
2021-10-30 16:29:52 +0300

    Modified

--- src/utils/string/decapitalizeFirstLetter.ts
+++ src/utils/string/decapitalizeFirstLetter.ts
@@ -1,3 +1,7 @@
 export default function decapitalizeFirstLetter(str: string): string {
+  if (str[0] === '_') {
+    return str[0] + str[1].toLowerCase() + str.slice(2);
+  }
+
   return str[0].toLowerCase() + str.slice(1);
 }

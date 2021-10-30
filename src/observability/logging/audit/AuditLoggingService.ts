1ceee8a1a7c43f96b6ce5e387eb87cc6cf86258a
Petri Silen <petri.silen@silensoft.com>
2021-10-30 11:12:14 +0300

    Modified

--- src/observability/logging/audit/AuditLoggingService.ts
+++ src/observability/logging/audit/AuditLoggingService.ts
@@ -2,5 +2,8 @@ import { AuditLogEntry } from "./AuditLogEntry";
 import BaseService from "../../../services/BaseService";
 
 export default abstract class AuditLoggingService extends BaseService {
+  constructor() {
+    super({});
+  }
   abstract async log(auditLogEntry: AuditLogEntry): Promise<void>;
 }

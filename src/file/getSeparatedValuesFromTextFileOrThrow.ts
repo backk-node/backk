6bb5dd7ced09c62c2751c91c1a054b67b7f87736
Petri Silen <petri.silen@silensoft.com>
2021-12-23 15:59:44 +0200

    Modified

--- src/file/getSeparatedValuesFromTextFileOrThrow.ts
+++ src/file/getSeparatedValuesFromTextFileOrThrow.ts
@@ -1,18 +1,12 @@
 import { readFileSync } from 'fs';
-import log, { Severity } from "../observability/logging/log";
 
 export default function getSeparatedValuesFromTextFileOrThrow(
   filePathNameRelativeToResourcesDir: string,
   separator = '\n'
 ): string[] {
-  try {
-    const fullPathName = process.cwd() + "/build/resources/" + filePathNameRelativeToResourcesDir;
-    return readFileSync(fullPathName, { encoding: 'UTF-8' })
-      .split(separator)
-      .filter((value) => value)
-      .map((value) => value.trim())
-  } catch(error) {
-    log(Severity.ERROR, error.message, '');
-    return []
-  }
+  const fullPathName = process.cwd() + '/build/resources/' + filePathNameRelativeToResourcesDir;
+  return readFileSync(fullPathName, { encoding: 'UTF-8' })
+    .split(separator)
+    .filter((value) => value)
+    .map((value) => value.trim());
 }

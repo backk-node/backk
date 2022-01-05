d4bfe786cf4093f84be68cbd151bd0ea3a126416
Petri Silen <petri.silen@silensoft.com>
2022-01-05 13:26:32 +0200

    Modified

--- src/client/generateClients.spec.ts
+++
@@ -1,8 +0,0 @@
-import generateClients from "./generateClients";
-
-describe('generateClients', () => {
-  it('should generate frontend and internal clients', () => {
-    process.env.MICROSERVICE_NAMESPACE = 'default';
-    // generateClients();
-  })
-})

cdbe3f49e2178ad03710682fa6e9348aa97feef3
Petri Silen <petri.silen@silensoft.com>
2022-01-05 16:42:09 +0200

    Modified

--- src/utils/changePackageJsonNameProperty.ts
+++ src/utils/changePackageJsonNameProperty.ts
@@ -9,25 +9,27 @@ export default async function changePackageJsonNameProperty() {
   try {
     const packageJsonContents = readFileSync('package.json', { encoding: 'UTF-8' });
     const packageJsonObject = JSON.parse(packageJsonContents);
-    const microserviceName = process.cwd().split('/').reverse()[0];
-    packageJsonObject.name = microserviceName;
-    const replaceConfig = {
-      files: [
-        'package.json',
-        'env.dev',
-        'env.ci',
-        'package-lock.json',
-        'sonar-project.properties',
-        '.github/workflows/ci.yaml',
-        'helm/backk-starter/Chart.yaml',
-        'helm/backk-starter/values.yaml',
-        'helm/values/values-minikube.yaml'
-      ],
-      from: /backk-starter/g,
-      to: microserviceName
+    if (packageJsonObject.name === 'backk-starter') {
+      const microserviceName = process.cwd().split('/').reverse()[0];
+      packageJsonObject.name = microserviceName;
+      const replaceConfig = {
+        files: [
+          'package.json',
+          'env.dev',
+          'env.ci',
+          'package-lock.json',
+          'sonar-project.properties',
+          '.github/workflows/ci.yaml',
+          'helm/backk-starter/Chart.yaml',
+          'helm/backk-starter/values.yaml',
+          'helm/values/values-minikube.yaml'
+        ],
+        from: /backk-starter/g,
+        to: microserviceName
+      }
+      await replaceInFile(replaceConfig);
+      renameSync('helm/backk-starter', 'helm/' + microserviceName);
     }
-    await replaceInFile(replaceConfig);
-    renameSync('helm/backk-starter', 'helm/' + microserviceName);
   } catch {
     // No operation
   }
